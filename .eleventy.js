0e7cc4a4752724ff2a8e924541a57547bca1ca0e
viktorstrate <viktorstrate@gmail.com>
2020-12-11 23:28:54 +0100

    Add text for extra features section

--- .eleventy.js
+++ .eleventy.js
@@ -1,38 +1,50 @@
-const pluginTailwindCSS = require("eleventy-plugin-tailwindcss")
-const htmlmin = require("html-minifier")
+const pluginTailwindCSS = require('eleventy-plugin-tailwindcss')
+const htmlmin = require('html-minifier')
+const markdownIt = require('markdown-it')
 
-module.exports = function(eleventyConfig) {
+const markdownOptions = {
+  html: true,
+}
 
+module.exports = function (eleventyConfig) {
   const isProduction = process.env.NODE_ENV == 'production'
 
   // Copy assets
-  eleventyConfig.addPassthroughCopy("src/assets")
+  eleventyConfig.addPassthroughCopy('src/assets')
 
   // Tailwind
   eleventyConfig.addPlugin(pluginTailwindCSS, {
-    src: "src/styles/main.css",
-    dest: "assets",
+    src: 'src/styles/main.css',
+    dest: 'assets',
     keepFolderStructure: false,
     minify: isProduction,
     watchEleventyWatchTargets: true,
   })
 
   // Minify HTML in production
-  eleventyConfig.addTransform("htmlmin", function(content, outputPath) {
-    if( isProduction && outputPath.endsWith(".html") ) {
+  eleventyConfig.addTransform('htmlmin', function (content, outputPath) {
+    if (isProduction && outputPath.endsWith('.html')) {
       let minified = htmlmin.minify(content, {
         useShortDoctype: true,
         removeComments: true,
-        collapseWhitespace: true
-      });
-      return minified;
+        collapseWhitespace: true,
+      })
+      return minified
     }
 
-    return content;
-  });
+    return content
+  })
 
   // Custom filters
-  eleventyConfig.addShortcode("paragraph", function(text, classes) {
-    return text.split('\n\n').map(par => `<p class="${classes}">${par}</p>`).join('\n')
+  eleventyConfig.addShortcode('paragraph', function (text, classes) {
+    return text
+      .split('\n\n')
+      .map(par => `<p class="${classes}">${par}</p>`)
+      .join('\n')
+  })
+
+  eleventyConfig.addFilter('markdown', function (text) {
+    const md = markdownIt(markdownOptions)
+    return md.render(text)
   })
 }
