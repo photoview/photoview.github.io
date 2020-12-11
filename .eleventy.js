fc6ccf830622030e4ce98e5fdea299f7c5dba073
viktorstrate <viktorstrate@gmail.com>
2020-12-11 18:09:37 +0100

    Make landing page

---
+++ .eleventy.js
@@ -0,0 +1,38 @@
+const pluginTailwindCSS = require("eleventy-plugin-tailwindcss")
+const htmlmin = require("html-minifier")
+
+module.exports = function(eleventyConfig) {
+
+  const isProduction = process.env.NODE_ENV == 'production'
+
+  // Copy assets
+  eleventyConfig.addPassthroughCopy("src/assets")
+
+  // Tailwind
+  eleventyConfig.addPlugin(pluginTailwindCSS, {
+    src: "src/styles/main.css",
+    dest: "assets",
+    keepFolderStructure: false,
+    minify: isProduction,
+    watchEleventyWatchTargets: true,
+  })
+
+  // Minify HTML in production
+  eleventyConfig.addTransform("htmlmin", function(content, outputPath) {
+    if( isProduction && outputPath.endsWith(".html") ) {
+      let minified = htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true
+      });
+      return minified;
+    }
+
+    return content;
+  });
+
+  // Custom filters
+  eleventyConfig.addShortcode("paragraph", function(text, classes) {
+    return text.split('\n\n').map(par => `<p class="${classes}">${par}</p>`).join('\n')
+  })
+}
---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  purge: [
+    './src/**/*.html',
+    './src/**/*.njk',
+  ],
+  theme: {
+    fontFamily: {
+      sans: ['Lato', 'sans-serif'],
+    },
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '972px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
+    extend: {
+      colors: {
+        primary: '#168fd4',
+        default: '#565656',
+      },
+      spacing: {
+        '572px': '572px',
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
