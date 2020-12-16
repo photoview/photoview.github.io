18dc4da97339aff8a5f2a5ca2aaa5346edc564a8
viktorstrate <viktorstrate@gmail.com>
2020-12-16 15:19:27 +0100

    Optimize images, improve layout

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 const pluginTailwindCSS = require('eleventy-plugin-tailwindcss')
 const htmlmin = require('html-minifier')
 const markdownIt = require('markdown-it')
+const Image = require('@11ty/eleventy-img')
 
 const markdownOptions = {
   html: true,
@@ -26,8 +27,41 @@ module.exports = function (eleventyConfig) {
   // Markdown
   setupMarkdown(eleventyConfig)
 
+  // Optimize images
+  eleventyConfig.addNunjucksAsyncShortcode(
+    'optimizedImage',
+    async (src, alt) => {
+      if (alt === undefined) {
+        throw new Error(`Missing \`alt\` on image shortcode from: ${src}`)
+      }
+
+      let stats = await Image(src, {
+        widths: [640, null],
+        formats: ['webp', 'jpeg'],
+        outputDir: './_site/assets/images',
+        urlPath: '/assets/images/',
+      })
+      let lowestSrc = stats['jpeg'][0]
+      let sizes = '100vw' // Make sure you customize this!
+
+      // Iterate over formats and widths
+      return `${Object.values(stats)
+        .map(imageFormat => {
+          return `  <source type="image/${
+            imageFormat[0].format
+          }" srcset="${imageFormat
+            .map(entry => entry.srcset)
+            .join(', ')}" sizes="${sizes}">`
+        })
+        .join('\n')}
+        <img
+          src="${lowestSrc.url}"
+          alt="${alt}">`
+    }
+  )
+
   // Minify HTML in production
-  eleventyConfig.addTransform('htmlmin', function (content, outputPath) {
+  eleventyConfig.addTransform('htmlmin', (content, outputPath) => {
     if (isProduction && outputPath.endsWith('.html')) {
       let minified = htmlmin.minify(content, {
         useShortDoctype: true,
@@ -41,14 +75,14 @@ module.exports = function (eleventyConfig) {
   })
 
   // Custom filters
-  eleventyConfig.addShortcode('paragraph', function (text, classes) {
+  eleventyConfig.addShortcode('paragraph', (text, classes) => {
     return text
       .split('\n\n')
       .map(par => `<p class="${classes}">${par}</p>`)
       .join('\n')
   })
 
-  eleventyConfig.addFilter('markdown', function (text) {
+  eleventyConfig.addFilter('markdown', text => {
     const md = markdownIt(markdownOptions)
     return md.render(text)
   })
@@ -57,17 +91,17 @@ module.exports = function (eleventyConfig) {
     console.log('DEBUG', obj)
   })
 
-  eleventyConfig.addFilter('docsNavGroupItems', function (group, collection) {
+  eleventyConfig.addFilter('docsNavGroupItems', (group, collection) => {
     return collection.filter(x => x.data.group == group)
   })
 }
 
-const setupMarkdown = config => {
+const setupMarkdown = eleventyConfig => {
   const linkAnchorOptions = {
     permalink: true,
     permalinkAttrs: () => ({ 'aria-label': 'Anchor' }),
     permalinkClass: 'header-anchor',
-    permalinkSymbol: '#',
+    permalinkSymbol: '',
     permalinkBefore: true,
   }
 
@@ -76,8 +110,8 @@ const setupMarkdown = config => {
     linkAnchorOptions
   )
 
-  config.setLibrary('md', md)
-  config.addFilter('markdown', markdown => {
+  eleventyConfig.setLibrary('md', md)
+  eleventyConfig.addFilter('markdown', markdown => {
     return md.render(markdown)
   })
 }
