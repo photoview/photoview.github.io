7b381d1ff6b316c3ccd60e7fe1d00a73b5884dd6
viktorstrate <viktorstrate@gmail.com>
2021-02-26 17:23:14 +0100

    Make documentation text slightly smaller

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,20 @@ module.exports = {
       xl: '1280px',
       '2xl': '1536px',
     },
+    fontSize: {
+      xs: '.75em',
+      sm: '.875em',
+      tiny: '.875em',
+      base: '1em',
+      lg: '1.125em',
+      xl: '1.25em',
+      '2xl': '1.5em',
+      '3xl': '1.875em',
+      '4xl': '2.25em',
+      '5xl': '3em',
+      '6xl': '4em',
+      '7xl': '5em',
+    },
     extend: {
       colors: {
         primary: '#168fd4',
